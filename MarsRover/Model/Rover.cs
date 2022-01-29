da0817b4e0d288e72838d948681f1ac2168bd033
mervepektas <mervpektas@hotmail.com>
2022-01-29 19:57:29 +0300

    *created project
    *created project

---
+++ MarsRover/Model/Rover.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsRover.Model
+{
+    public class Plateau
+    {
+        public int min_Width { get; set; }
+
+        public int min_Height { get; set; }
+
+        public int width { get; set; }
+
+        public int height { get; set; }
+    }
+    public class Position
+    {
+        public int x { get; set; }
+
+        public int y { get; set; }
+    }
+    public enum DirectionEnum
+    {
+        N = 1,
+        E = 2,
+        S = 3,
+        W = 4
+    }
+    public class Rover
+    {
+        public Plateau plateau { get; set; }
+
+        public Position position { get; set; }
+
+        public DirectionEnum direction { get; set; }
+    }
+}
---
+++ MarsRover/Program.cs
@@ -0,0 +1,66 @@
+﻿using MarsRover.Model;
+using System;
+
+namespace MarsRover
+{
+    public static class Program
+    {
+        public static DirectionEnum direction = DirectionEnum.N;        
+        public static void Process(this Rover rover, string command)
+        {
+            for (int i = 0; i < command.Length; i++)
+            {
+                if ('L' == command[i])
+                    direction = ((int)direction - 1) < (int)DirectionEnum.N ? DirectionEnum.W : (DirectionEnum)((int)direction - 1);
+                else if ('R' == command[i])
+                    direction = ((int)direction + 1) > (int)DirectionEnum.W ? DirectionEnum.N : (DirectionEnum)((int)direction + 1);
+                else if ('M' == command[i])
+                {
+                    rover.Move();
+                }
+            }
+        }
+        public static bool Move(this Rover rover)
+        {
+            switch (direction)
+            {
+                case DirectionEnum.N:
+                    rover.position.y += 1;
+                    break;
+                case DirectionEnum.E:
+                    rover.position.x += 1;
+                    break;
+                case DirectionEnum.S:
+                    rover.position.y -= 1;
+                    break;
+                case DirectionEnum.W:
+                    rover.position.x -= 1;
+                    break;
+            }
+
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            Plateau plateau = new Plateau();
+            plateau.height = 5;
+            plateau.width = 5;
+            Position position = new Position();
+            position.x = 1;
+            position.y = 2;
+
+            Rover rover = new Rover();
+            rover.plateau = plateau;
+            rover.position = position;
+            rover.direction = DirectionEnum.N;
+            rover.Process("LMLMLMLMM");
+            Console.WriteLine(rover.position.x + " " + rover.position.y + " " + rover.direction); // 1 3 N
+
+            rover.position.x = 3;
+            rover.position.y = 3;
+            rover.direction = DirectionEnum.E;       
+            rover.Process("MMRMMRMRRM");
+            Console.WriteLine(rover.position.x + " " + rover.position.y + " " + rover.direction); //5 1 E
+        }
+    }
+}
